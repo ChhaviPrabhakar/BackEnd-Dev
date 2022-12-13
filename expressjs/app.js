00683c5733d8afecd874fde4f3fde4c049ea999b
Chhavi Prabhakar <prabhakarchhavi9@gmail.com>
2022-12-13 23:43:13 +0530

    task-3

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,19 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('this always run');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/add-product', (req, res, next) => {
-    console.log('in Middleware!');
-    res.send('<h1>the Add-Product page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('in another Middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
